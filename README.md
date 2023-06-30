eecec07cca11cceba15c351757b8e831c2a21135
Emad_HP_Home <e.a.darwich@gmail.com>
2023-07-01 00:55:55 +0200

    feat:

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# LightGlue_streamlit
+# LightGlue Streamlit Application
+
+This Streamlit application demonstrates the usage of LightGlue, a new technology that makes it easier and quicker to find similarities between pictures. It allows you to choose two images and find their similarities by clicking the "Matching" button.
+
+## Getting Started
+
+To run this application locally, follow the instructions below:
+
+1. Clone the repository:
+
+   ```shell
+   git clone https://github.com/emaddar/LightGlue_streamlit.git
+   ```
+
+2. Install the required dependencies:
+    ```shell
+        pip install -r requirements.txt
+    ```
+3. Run the application:
+    ```shell
+    streamlit run app.py
+    ```
+4. The application will be accessible at http://localhost:8501 in your web browser.
+
+
+## Usage
+### Upload Image 1:
+
+- Click on the "Upload your image 1" button and select an image file (PNG, JPEG, or JPG format).
+- The selected image will be displayed on the left side of the application.
+
+### Upload Image 2:
+
+
+
+- Click on the "Upload your image 2" button and select another image file.
+- The selected image will be displayed on the right side of the application.
+
+### Click the "Matching" button:
+
+- After uploading both images, click on the "Matching" button to find the similarities between them.
+- The application will display the matched keypoints and visualize the similarities between the images.
+
+
+## References
+## References
+
+- [LightGlue: Local Feature Matching at Light Speed](https://arxiv.org/pdf/2306.13643.pdf) - Lindenberger, P., Sarlin, P.-E., & Pollefeys, M.
+
+- [LightGlue: (GitHub Repo)](https://github.com/cvg/lightglue?utm_source=tldrai)
+---
+
+This Streamlit application is developed using the LightGlue technology to showcase its capabilities in finding similarities between images. Feel free to explore and experiment with different image pairs to discover their similarities!
+
--- app.py
+++ app.py
@@ -6,48 +6,9 @@ from lightglue import viz2d
 from pathlib import Path
 import torch
 from lightglue import LightGlue, SuperPoint, DISK
-
-
-
-from pathlib import Path
-from setuptools import setup
-
-description = ['LightGlue']
-
-with open(str(Path(__file__).parent / 'README.md'), 'r', encoding='utf-8') as f:
-    readme = f.read()
-
-extra_dependencies = ['torch', 'kornia', 'numpy', 'einops']
-
-setup(
-    name='lightglue',
-    version='0.0',
-    packages=['lightglue'],
-    python_requires='>=3.6',
-    extras_require={'extra': extra_dependencies},
-    author='Philipp Lindenberger, Paul-Edouard Sarlin',
-    description=description,
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    url='https://github.com/cvg/LightGlue/',
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-    ],
-)
-
-
-
-extractor = SuperPoint(max_num_keypoints=2048, nms_radius=3).eval()  # load the extractor
-match_conf = {
-    'width_confidence': 0.99,  # for point pruning
-    'depth_confidence': 0.95,  # for early stopping,
-}
-matcher = LightGlue(pretrained='superpoint', **match_conf).eval()
-
-
-st.write('Hello')
+import os
+import uuid
+from PIL import Image
 
 
 # SuperPoint+LightGlue
@@ -58,23 +19,118 @@ matcher = LightGlue(pretrained='superpoint').eval() # load the matcher
 extractor = DISK(max_num_keypoints=1000).eval() # load the extractor
 matcher = LightGlue(pretrained='disk').eval() # load the matcher
 
-# load images to torch and resize to max_edge=1024
-image0, scales0 = load_image("assets/Lille_Fladre0.jpg")
-image1, scales1 = load_image("assets/Lille_Fladre1.jpeg")
-
-# extraction + matching + rescale keypoints to original image size
-pred = match_pair(extractor, matcher, image0, image1,
-                  scales0=scales0, scales1=scales1)    
-
-kpts0, kpts1, matches = pred['keypoints0'], pred['keypoints1'], pred['matches']
-m_kpts0, m_kpts1 = kpts0[matches[..., 0]], kpts1[matches[..., 1]]
 
 
+@st.cache_data
+def save_uploadedfile(uploadedfile, image_name):
+     with open(os.path.join("assets/images",image_name),"wb") as f:
+         f.write(uploadedfile.getbuffer())
 
 
-axes = viz2d.plot_images([image0.permute(1, 2, 0), image1.permute(1, 2, 0)])
-viz2d.plot_matches(m_kpts0, m_kpts1, color='lime', lw=0.2)
+extractor = SuperPoint(max_num_keypoints=2048, nms_radius=3).eval()  # load the extractor
+match_conf = {
+    'width_confidence': 0.99,  # for point pruning
+    'depth_confidence': 0.95,  # for early stopping,
+}
 
-viz2d.save_plot('assets/save1.jpg')
+# make any grid with a function
+def make_grid(cols,rows):
+    grid = [0]*cols
+    for i in range(cols):
+        with st.container():
+            grid[i] = st.columns(rows)
+    return grid
 
-st.image('assets/save1.jpg')
+
+
+st.set_page_config(layout="wide")
+st.header("LightGlue: A Smarter, Faster Image Matching Technique ")
+
+
+st.write(" ")
+st.write(" ")
+
+st.write("Please select two images")
+
+extractor = SuperPoint(max_num_keypoints=2048, nms_radius=3).eval()  # load the extractor
+match_conf = {
+    'width_confidence': 0.99,  # for point pruning
+    'depth_confidence': 0.95,  # for early stopping,
+}
+matcher = LightGlue(pretrained='superpoint', **match_conf).eval()
+
+
+
+
+
+# Generate a unique key for each widget
+key1 = str(uuid.uuid4())
+key2 = str(uuid.uuid4())
+
+
+
+# Define the column layout
+col1, col2 = st.columns(2)
+
+with col1:
+    uploaded_file1 = st.file_uploader("Upload your image 1", type=['png', 'jpeg', 'jpg'], key='key1')
+
+    if uploaded_file1 is not None:
+        st.image(uploaded_file1)
+        save_uploadedfile(uploaded_file1, "image0.jpg")
+        
+with col2:
+    uploaded_file2 = st.file_uploader("Upload your image 2", type=['png', 'jpeg', 'jpg'], key='key2')
+
+    if uploaded_file2 is not None:
+        st.image(uploaded_file2)
+        save_uploadedfile(uploaded_file2, "image1.jpg")
+
+mygrid0 = make_grid(1,3)
+
+if uploaded_file2 is not None and uploaded_file2 is not None:
+    mybutton = mygrid0[0][1].button('Matching')
+
+    if mybutton:
+      with st.spinner("Matching ..."):
+
+        # SuperPoint+LightGlue
+        extractor = SuperPoint(max_num_keypoints=1000).eval()  # load the extractor
+        matcher = LightGlue(pretrained='superpoint').eval() # load the matcher
+
+        # or DISK+LightGlue
+        extractor = DISK(max_num_keypoints=1000).eval() # load the extractor
+        matcher = LightGlue(pretrained='disk').eval() # load the matcher
+
+        # load images to torch and resize to max_edge=1024
+        image0, scales0 = load_image("assets/images/image0.jpg")
+        image1, scales1 = load_image("assets/images/image1.jpg")
+
+        # extraction + matching + rescale keypoints to original image size
+        pred = match_pair(extractor, matcher, image0, image1,
+                        scales0=scales0, scales1=scales1)    
+
+        kpts0, kpts1, matches = pred['keypoints0'], pred['keypoints1'], pred['matches']
+        m_kpts0, m_kpts1 = kpts0[matches[..., 0]], kpts1[matches[..., 1]]
+
+
+
+
+        axes = viz2d.plot_images([image0.permute(1, 2, 0), image1.permute(1, 2, 0)])
+        viz2d.plot_matches(m_kpts0, m_kpts1, color='lime', lw=0.2)
+
+        viz2d.save_plot('assets/save1.jpg')
+
+        st.image('assets/save1.jpg')
+
+
+        
+
+st.write(" ")
+st.write(" ")
+st.write(" ")
+
+st.write("---")
+st.write("References:")
+st.write("[1] Lindenberger, P., Sarlin, P.-E., & Pollefeys, M. (2023). LightGlue: Local Feature Matching at Light Speed. ArXiv PrePrint.")
+
