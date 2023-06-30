cf8977b42ab0a76808a25fb2e2b24e825b50aa1e
Emad_HP_Home <e.a.darwich@gmail.com>
2023-06-30 23:07:56 +0200

    feat:

--- app.py
+++ app.py
@@ -9,6 +9,36 @@ from lightglue import LightGlue, SuperPoint, DISK
 
 
 
+from pathlib import Path
+from setuptools import setup
+
+description = ['LightGlue']
+
+with open(str(Path(__file__).parent / 'README.md'), 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+extra_dependencies = ['torch', 'kornia', 'numpy', 'einops']
+
+setup(
+    name='lightglue',
+    version='0.0',
+    packages=['lightglue'],
+    python_requires='>=3.6',
+    extras_require={'extra': extra_dependencies},
+    author='Philipp Lindenberger, Paul-Edouard Sarlin',
+    description=description,
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url='https://github.com/cvg/LightGlue/',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
+
 extractor = SuperPoint(max_num_keypoints=2048, nms_radius=3).eval()  # load the extractor
 match_conf = {
     'width_confidence': 0.99,  # for point pruning
